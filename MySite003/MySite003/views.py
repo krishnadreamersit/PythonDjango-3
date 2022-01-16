10f9ab00cbe8da39e9b7e1006fec3796e8638283
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-01-16 19:16:22 +0545

    Jan 16, 2022

--- MySite003/MySite003/views.py
+++ MySite003/MySite003/views.py
@@ -1,12 +1,21 @@
 # Step-1 - Import Library
-from django.http import HttpResponse
+from django.http import HttpResponse # return string as html
+from django.shortcuts import render # return html file as html
 
 # Step-2 - create new function
 def home(request):
-    return HttpResponse("Welcome to Broadway InfoSys Nepal")
+    str1 = "<html><head><style>body{padding-top:50pt;}</style><title>Welcome to broadwayinfosys.com</title></head><body bgcolor='blue' text='white'>"
+    str1 = str1 + "<h1>Building Global IT Professionals since 2008</h1>"
+    str1 = str1 +"<h3>AN ISO 9001:2015 CERTIFIED IT LEARNING CENTER</h3>"
+    str1 = str1 +"<p>Broadway Infosys Nepal is one of the best inclusive computer training institutes in Kathmandu, Nepal. Established in 2008, our professional IT Training and Development center has been employing experts in this field to impart professional education.</p>"
+    str1 = str1 +"</body></html>"
+    return HttpResponse(str1)
+
 
 def successgallery(request):
-    return HttpResponse("Success Galleries")
+    return render(request, 'successgallery.html')
+    # return HttpResponse("Success Galleries")
+
 
 def testimonials(rerquest):
     return HttpResponse("Tentimonials")
