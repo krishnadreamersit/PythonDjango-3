bebc4c1d771398100d50351cfe41f6a06b63f5c2
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-01-25 18:36:14 +0545

    Jan 25 2022 6:36 PM

--- Mysite301/app1_2/views.py
+++ Mysite301/app1_2/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -14,3 +15,10 @@ def index(request):
 def get_value2(request, pid):
     context = {'pid': pid}
     return render(request, 'app1_2/display2.html', context)
+
+
+def display_links(request):
+    # str1 = "<p><a href='../getvalues1?id=1&name=Bikki'>Send Value1</a></p>"
+    # str1 += "<p><a href='../getvalues2/1'>Send Value2</a></p>"
+    # return HttpResponse(str1)
+    return render(request, 'app1_2/links.html')
