5d8b3eed240a3692435ce1cd224dbfde24341445
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-02-23 18:56:39 +0545

    Feb 23, 2022

--- Mysite301/app6_1/views.py
+++ Mysite301/app6_1/views.py
@@ -1,12 +1,80 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
     # values - source (static, url, web form, file, database, network, api)
+    """
     id=1
     name="Bikki Goit"
     address ="KTM"
     office = "Broadway InfoSys"
     context = {'id':id, 'name':name, 'address':address, 'office': office}
     return render(request, 'app6_1/index.html', context)
+    """
+    # Tags
+
+    # autoescape
+    """
+    context = {'var1': '<strong>Welcome to Broadway</strong>'}
+    return render(request, 'app6_1/index.html', context)
+    """
+    # comment
+    # csrf_token # web form
+    # debug # settings.py -> DEBUG = True
+
+    # cycle
+    # for loop
+    """
+    persons = [
+        {'id':1, 'name':'Bikki Goit', 'address':'KTM'},
+        {'id':2, 'name':'Sanjaya Sharma', 'address':'LAT'},
+        {'id':3, 'name':'Tript Gurung', 'address':'BHK'},
+        {'id':4, 'name':'Krishna Aryal', 'address':'KTM'},
+    ]
+    context = {'persons':persons}
+    return render(request, 'app6_1/index2.html', context)
+    """
+
+    # firstof
+    """
+    var1 = None
+    var2 = None
+    var3 = None
+    return render(request, 'app6_1/index.html', {'var1':var1, 'var2':var2, 'var3':var3})
+    """
+
+    # for … empty
+    """
+    persons = [
+        {'id': 1, 'name': 'Bikki Goit', 'address': 'KTM'},
+        {'id': 2, 'name': 'Sanjaya Sharma', 'address': 'LAT'},
+        {'id': 3, 'name': 'Tript Gurung', 'address': 'BHK'},
+        {'id': 4, 'name': 'Krishna Aryal', 'address': 'KTM'},
+    ]
+    return render(request, 'app6_1/index.html', {'persons': persons})
+    """
+
+    # if
+    """
+    values={'n1':34, 'n2':43}
+    return render(request, 'app6_1/index.html', {'values': values})
+    """
+
+    # ifchanged
+    # ?
+
+    # extends
+    # block
+    """
+    return render(request, 'app6_1/child.html')
+    """
+
+    # include
+    return render(request, 'app6_1/body.html')
+
+    # load ?
+    # regroup ? # Record Grouping
+
+
